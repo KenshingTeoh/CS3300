c9661aa92cf3262ed68ac8746de87f244c31d5a7
Ken Teoh <kteoh@uccs.edu>
2021-11-15 21:05:21 +0000

    Testing with Validates_presence_of :title, :description

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ApplicationRecord
     # Check if it is empty for title and description while creating a new project
-    validates_presence_of :title, :description
+    # validates_presence_of :title, :description
 end
